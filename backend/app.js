05eef99f6d93cd2d49da9ab032d4dc7b02f9d36c
Oleg Volkov <olegv3@gmail.com>
2022-12-14 16:44:56 -0500

    Phase 2

--- backend/app.js
+++ backend/app.js
@@ -6,33 +6,27 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
-
-
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const { ValidationError } = require('sequelize');
 
 
 const app = express();
 
-
 app.use(morgan('dev'));
-
 
 app.use(cookieParser());
 app.use(express.json());
 
-
 if (!isProduction) {
     app.use(cors());
 }
-
 
 app.use(
     helmet.crossOriginResourcePolicy({
         policy: "cross-origin"
     })
 );
-
 
 app.use(
     csurf({
@@ -44,11 +38,35 @@ app.use(
     })
 );
 
-
 app.use(routes);
 
 
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
 
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
 
 
--- backend/notes.js
+++
@@ -1,4 +0,0 @@
-// npm install -D the following packages as dev-dependencies:
-// sqlite3 - SQLite3
-// dotenv-cli - use dotenv in the command line
-// nodemon - hot reload server backend files
