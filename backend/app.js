2cf38baee303f73d095181a9c4d66c2362e71436
Oleg Volkov <olegv3@gmail.com>
2022-12-14 17:16:25 -0500

    Phase 2

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,8 @@ app.use((_req, _res, next) => {
     next(err);
   });
 
+// const { ValidationError } = require('sequelize');
+
 app.use((err, _req, _res, next) => {
   if (err instanceof ValidationError) {
     err.errors = err.errors.map((e) => e.message);
