96ca60ab11775047510be5ee61c4316db6bf6ac9
Oleg Volkov <olegv3@gmail.com>
2022-12-15 17:35:00 -0500

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -52,17 +52,17 @@ router.delete(
 );
 
 // // Restore session user
-// router.get(
-//     '/',
-//     restoreUser,
-//     (req, res) => {
-//         const { user } = req;
-//         if (user) {
-//             return res.json({
-//                 user: user.toSafeObject()
-//             });
-//         } else return res.json({ user: null });
-//     }
-// );
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({ user: null });
+    }
+);
 
 module.exports = router;
