9bbf37918df7ddd586e44387a3ea2d541b72e9ef
Oleg Volkov <olegv3@gmail.com>
2022-12-20 12:21:45 -0500

    auth fix

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -26,25 +26,25 @@ router.post('/test', function (req, res) {
 });
 
 // GET /api/set-token-cookie
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         }
-//     });
-//     setTokenCookie(res, user);
-//     return res.json({ user: user });
-// });
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
+});
 
 // GET /api/restore-user
-// router.get(
-//     '/restore-user',
-//     (req, res) => {
-//         return res.json(req.user);
-//     }
-// );
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
 const { requireAuth } = require('../../utils/auth.js');
 router.get(
